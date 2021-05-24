45929e7548904acda572699ab476ef8ac78ad737
Longji Li <fdbj14@gmail.com>
2021-05-24 09:32:39 +0800

    Connected with firebase and created sign up, log in forms

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="submitHandler">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Log In</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const email = ref('')
+    const password = ref('')
+
+    const submitHandler = () => {
+      console.log(email.value, password.value)
+    }
+
+    return { email, password, submitHandler }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="submitHandler">
+    <input
+      type="text"
+      required
+      placeholder="display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const displayName = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    const submitHandler = () => {
+      console.log(displayName.value, email.value, password.value)
+    }
+
+    return { displayName, email, password, submitHandler }
+  }
+}
+</script>
+
+<style></style>
