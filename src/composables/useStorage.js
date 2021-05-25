9987be2a9d96265d61a8eb8d134d714e152a3f10
Longji Li <fdbj14@gmail.com>
2021-05-25 08:28:38 +0800

    Removed unnecessary comments

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -31,25 +31,4 @@ const useStorage = (dir) => {
   return { error, addItem }
 }
 
-// getDownloadURL(ref(storage, 'images/stars.jpg'))
-//   .then((url) => {
-//     // `url` is the download URL for 'images/stars.jpg'
-
-//     // This can be downloaded directly:
-//     const xhr = new XMLHttpRequest();
-//     xhr.responseType = 'blob';
-//     xhr.onload = (event) => {
-//       const blob = xhr.response;
-//     };
-//     xhr.open('GET', url);
-//     xhr.send();
-
-//     // Or inserted into an <img> element
-//     const img = document.getElementById('myimg');
-//     img.setAttribute('src', url);
-//   })
-//   .catch((error) => {
-//     // Handle any errors
-//   });
-
 export default useStorage
